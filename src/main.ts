2b97ad380f4482c25ac8a463334b887abcd01c65
“zgsyxy” <zgs15800620576@163.com>
2024-05-25 20:07:54 +0800

    feat: 项目配置相关

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import '@/styles'
 import App from './App.vue'
+import 'virtual:svg-icons-register'
+import gloablComponent from './components/SvgIcon';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+app.use(gloablComponent)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.mount('#app')
